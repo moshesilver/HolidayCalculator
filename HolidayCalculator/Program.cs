c5c819673c3a0d94e491067d88938a0444e4fd40
moshesilver <moshetzvi03@gmail.com>
2023-07-26 10:43:44 -0400

    everything works cleanup time :)

--- HolidayCalculator/Program.cs
+++ HolidayCalculator/Program.cs
@@ -6,7 +6,7 @@ namespace HolidayCalculator
     {
         static void Main(string[] args)
         {
-            HolidayCalculator holidayCalculator = new HolidayCalculator();
+            HolidayCalculator holidayCalculator = new();
             Console.WriteLine("Enter a year");
             int chosenYear = Convert.ToInt32(Console.ReadLine());
             holidayCalculator.CalculateHolidays(chosenYear);
@@ -35,10 +35,10 @@ namespace HolidayCalculator
         public List<DateTime> CalculateHolidays(int year)
         {
             //LATER ADD MONTH VARIABLE SO I CAN REUSE FUNCTIONS AND VARIABLES
-            List<DateTime> holidays = new List<DateTime>();
+            List<DateTime> holidays = new();
 
-            //MEMORIAL DAY
-            DateTime firstDayOfMay = new DateTime(year, 5, 1);
+
+
 
             //replace 5 with 'month'
             //replace May with 'Month'
@@ -54,39 +54,40 @@ namespace HolidayCalculator
             int lastMonInMay = lastSunInMay + 1;
             if (lastMonInMay > 31) { lastMonInMay -= week; }*/
 
-            int daysToSun = 7 - Convert.ToInt32(firstDayOfMay.DayOfWeek);
-            if (daysToSun == 7) { daysToSun = 0; }
-            int daysAfterSunInMay = 31 - daysToSun;
-            int lastMonInMay = ((daysToSun + 1) + (daysAfterSunInMay -= (daysAfterSunInMay % 7))) + 1;
+            //MEMORIAL DAY
+            DateTime firstDayOfMay = new (year, 5, 1);
+            int daysToMon = (7 - Convert.ToInt32(firstDayOfMay.DayOfWeek)) + 1;
+            if (daysToMon >= 7) { daysToMon -= 7; }
+            int daysAfterMonInMay = 31 - daysToMon;
+            int lastMonInMay = ((daysToMon + 1) + (daysAfterMonInMay -= (daysAfterMonInMay % 7)));
             if (lastMonInMay > 31) { lastMonInMay -= 7; }
 
             //may has 31 days so for the last week in may substract daysToSun(without the +1) from 31
             //then divide by 7 which will tell you how many weeks there are in may starting from the first sunday
             //then you should add that many weeks of days to sunday to get the last sunday in may
 
-            DateTime MemorialDay = new DateTime(year, 5, lastMonInMay);//LATER REMOVE THIS LINE AND PUT DIRECTLY INTO holidays.Add()
+            DateTime MemorialDay = new (year, 5, lastMonInMay);//LATER REMOVE THIS LINE AND PUT DIRECTLY INTO holidays.Add()
 
             // Console.WriteLine($"Memorial Day in {year} is: May {lastMonInMay}th!");//for testing memorial day
 
             //LABOR DAY
-            int monday;
-            DateTime firstDayOfSeptember = new DateTime(year, 9, 1);
-            if (firstDayOfSeptember.DayOfWeek == DayOfWeek.Monday) { monday = 1; }
-            else
-            {
-                int daysToMon = (7 - Convert.ToInt32(firstDayOfSeptember.DayOfWeek)) + 1;
-                if (daysToMon == 8) { daysToMon = 1; }
-                else if (daysToMon == 7) { daysToMon = 0; }//just in case (should be impossible)
-                monday = daysToMon + 1;
-            }
+            DateTime firstDayOfSeptember = new (year, 9, 1);
+            int daysToMon_ = (7 - Convert.ToInt32(firstDayOfSeptember.DayOfWeek)) + 1;
+            if (daysToMon_ >= 7) { daysToMon_ -= 7; }
+            int monday = daysToMon_ + 1;
+            
+            DateTime LaborDay = new (year, 9, monday);//LATER REMOVE THIS LINE AND PUT DIRECTLY INTO holidays.Add()
 
-            DateTime LaborDay = new DateTime(year, 9, monday);//LATER REMOVE THIS LINE AND PUT DIRECTLY INTO holidays.Add()
+            //THANKSGIVING
+            DateTime firstDayOfNov = new (year, 11, 1);
+            int daysToThurs = (7 - Convert.ToInt32(firstDayOfNov.DayOfWeek)) + 4;
+            if (daysToThurs >= 7) { daysToThurs -= 7; }
+            int thursday = daysToThurs + 1;
+            int fourthThursInNov = thursday + 21;
+
+            DateTime Thanksgiving = new (year, 11, fourthThursInNov);//LATER REMOVE THIS LINE AND PUT DIRECTLY INTO holidays.Add()
 
 
-
-
-
-            //REMEMBER TO REMOVE TIMES (ONLY RETURN DATES)
 
 
             holidays.Add(MemorialDay);//Memorial Day
@@ -95,19 +96,17 @@ namespace HolidayCalculator
 
             holidays.Add(LaborDay);//Labor Day
 
+            holidays.Add(Thanksgiving);//Thanksgiving
+
             holidays.Add(new DateTime(year, 12, 25));//Christmas Day
 
-            holidays.Add(new DateTime(year, 1, 1));//New Year's Day MAYBE SHOULD BE FOR NEXT YEAR BECAUSE YEAR STARTS AFTER THIS
-                                                   //MAYBE SAY year + 1
-            /*
-
-            holidays.Add(new DateTime(year, 11, ));//Thanksgiving
-
-            */
+            holidays.Add(new DateTime((year + 1), 1, 1));//New Year's Day
+            
+            
 
             foreach (var holiday in holidays)
             {
-                Console.WriteLine(holiday.ToString());//LATER ADD ACCOMPANIED HOLIDAY NAMES IN LIST
+                Console.WriteLine(holiday.ToShortDateString());//LATER ADD ACCOMPANIED HOLIDAY NAMES IN LIST
             }
 
             return holidays;

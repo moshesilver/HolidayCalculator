37e42100a3c2c4460c7c80f5b08c35021b2426e7
moshesilver <moshetzvi03@gmail.com>
2023-08-03 12:00:36 -0400

    Now a working web app!!!

---
+++ OffDayCalcWebApp/Pages/Index.cshtml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using HolidayCalculator;
+
+namespace OffDayCalcWebApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public List<String> Result { get; private set; } = new List<String>();
+
+        public void OnPost()
+        {
+            string userInput = Request.Form["userInput"];
+
+            if (int.TryParse(userInput, out int year))
+            {
+                OffDayCalculator holidayCalculator = new(year);
+                List<DateTime> holidayOffDays = holidayCalculator.CalculateHolidayOffDays();
+                int i = 0;
+                foreach (DateTime offDay in holidayOffDays)
+                {
+                    Result.Add($"{holidayCalculator.holidayNames[i]}: {offDay.ToShortDateString()}");
+                    i++;
+                }
+            }
+        }
+    }
+}
